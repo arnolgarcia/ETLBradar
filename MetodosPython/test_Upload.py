753c2dc96f0fb9c0f7b80327dbd5b823a1da3977
arnolgarcia <arnol.garcia86@gmail.com>
2016-05-31 11:04:01 -0400

    script bradar online

--- MetodosPython/test_Upload.py
+++ MetodosPython/test_Upload.py
@@ -10,7 +10,7 @@ GlobalValues['host'] = "" + "152.231.85.227"
 GlobalValues['port'] = "" + "5433"
 GlobalValues['dbname'] = "" + "geoalert_testing"
 GlobalValues['user'] = "" + "postgres"
-GlobalValues['password'] = "" + "Admin321" #"admin"
+GlobalValues['password'] = "" + "Admin321"  # "admin"
 
 
 
@@ -20,12 +20,13 @@ GlobalValues['connString'] = "PG: host='%s' port='%s' dbname='%s' user='%s' pass
                                                                                   GlobalValues['dbname'],
                                                                                   GlobalValues['user'],
                                                                                   GlobalValues['password'])
-
+"""
 #connStr = GlobalValues['connString']
 #path = "C:/Users/Arnol/Desktop/Datos Bradar/data_files/processed"
 #path = "C:/Users/Arnol/GitHub/ETLBradar/MetodosPython/data_test"
 #tabla = "radar_bradar.uploaded_data"
 #radar = 1
+"""
 
 """
 ini_time = datetime.datetime.now()
@@ -42,7 +43,7 @@ connStr = GlobalValues['connString']
 tabla = "radar_bradar.data_radar_timeseries_test_online"
 
 datos = test.GetDataTest(filen)
-datos = sorted(datos, key = lambda tup: tup[3])
+datos = sorted(datos, key=lambda tup: tup[3])
 print("Datos leidos...")
 test.LoadDataBySQLwithSleep(connStr, tabla, datos, 10)
 
